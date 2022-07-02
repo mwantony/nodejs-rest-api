413fcba5f34d28d0908c93cfaf9eb8f1df18ba7b
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-02 14:57:04 -0300

    cap 2 vid 3 finalizado

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
+const path = require('path')
+
 
 module.exports = (caminho, nomeDoArquivo, callBackImagemCriada) => {
+  const tiposValidos = ['jpg', 'png', 'jpeg']
+  const tipo = path.extname(caminho)
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
 
-  const novoCaminho = `./assets/img/${nomeDoArquivo}`
-  
+  if(tipoEhValido === -1) {
+    console.log('Erro! Tipo inválido')
+    return
+  } 
+
+  const novoCaminho = `./assets/img/${nomeDoArquivo}${tipo}`
   fs.createReadStream(caminho)
     .pipe(fs.createWriteStream(novoCaminho))
     .on("finish", () => {
