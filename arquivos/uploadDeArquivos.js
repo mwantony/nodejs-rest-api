a27d4776f448133a36d65ba045a16279ff39abfe
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-02 13:59:32 -0300

    cap 1 finalizado

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
 
-fs.readFile('./assets/salsicha.jpg', (erro, buffer) => {
-  console.log('Imagem foi bufferizada.')
-
-  fs.writeFile('./assets/salsicha2.jpg', buffer, (erro) => {
-    console.log('Imagem foi escrita')
+fs.createReadStream('./assets/salsicha.jpg')
+  .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
+  .on('finish', () => {
+    console.log('Imagem foi escrita com sucesso!')
   })
-})
